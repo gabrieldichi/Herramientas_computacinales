1e7d673961824b97ba0fa35847ad0fda83a1616c
gabrieldichi <a01027301@itesm.mx>
2020-10-28 13:09:47 -0600

    funcion de rectangulo en paint.py

--- paint.py
+++ paint.py
@@ -39,7 +39,20 @@ def circle(start, end):
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward((end.x) - (start.x)*2)
+        left(90)
+
+    end_fill()
+    
+    pass # TODO
 
 def triangle(start, end):
     "Draw triangle from start to end."
