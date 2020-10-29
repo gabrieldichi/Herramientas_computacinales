e8eb71c04f572fb4a6964596d80ef48690850f42
Sebastian JP <sebjp@MacBook-Pro-de-Sebastian.local>
2020-10-29 12:45:27 -0600

    Modify board and snake pac-man.py and snake.py

--- snake.py
+++ snake.py
@@ -8,12 +8,11 @@ Exercises
 4. Change the snake to respond to arrow keys.
 
 """
-
+import random
 from turtle import *
 from random import randrange
 from freegames import square, vector
-
-food = vector(0, 0)
+food = vector(0,0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
@@ -21,14 +20,14 @@ def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
-
+      
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
 def move():
     "Move snake forward one segment."
-    head = snake[-1].copy()
+    head= snake[-1].copy()  
     head.move(aim)
 
     if not inside(head) or head in snake:
@@ -42,6 +41,8 @@ def move():
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
+       
+	
     else:
         snake.pop(0)
 
@@ -64,3 +65,4 @@ onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
 done()
+
