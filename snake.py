35e29f4797bee32589e6eb2e59a9f7acca092a5d
Sebastian JP <sebjp@MacBook-Pro-de-Sebastian.local>
2020-10-30 10:45:01 -0600

    Modify move keys snake.py

--- snake.py
+++ snake.py
@@ -20,15 +20,16 @@ def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
-      
+
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
 def move():
     "Move snake forward one segment."
-    head= snake[-1].copy()  
+    head= snake[-1].copy()
     head.move(aim)
+    i=0
 
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
@@ -41,8 +42,6 @@ def move():
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
-       
-	
     else:
         snake.pop(0)
 
@@ -54,15 +53,16 @@ def move():
     square(food.x, food.y, 9, 'green')
     update()
     ontimer(move, 100)
+    
 
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
+"Snake move with alternative keys"
+onkey(lambda: change(10, 0), 'd')
+onkey(lambda: change(-10, 0), 'a')
+onkey(lambda: change(0, 10), 'w')
+onkey(lambda: change(0, -10), 's')
 move()
 done()
-
