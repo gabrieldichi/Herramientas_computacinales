d88e416609d0981ff0f3d1586e87af162bf22235
gabrieldichi <57368147+gabrieldichi@users.noreply.github.com>
2020-10-30 11:00:54 -0600

    Update paint.py

--- paint.py
+++ paint.py
@@ -47,20 +47,7 @@ def circle(start, end):
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
-
-    for count in range(2):
-        forward(end.x, start.x)
-        left(90)
-        forward((end.x)*2 - (start.x)*2)
-        left(90)
-
-    end_fill()
-
-   	
+  
 
 def triangle(start, end):
     "Draw triangle from start to end."
