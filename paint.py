d7a4e10bff8f3410934f97a9000e0a588adc827a
Sebastian JP <sebjp@MacBook-Pro-de-Sebastian.local>
2020-10-30 09:12:12 -0600

    modify rectangle erase paint.py

--- paint.py
+++ paint.py
@@ -32,6 +32,7 @@ def square(start, end):
         left(90) 
     end_fill()
 
+
 def circle(start, end):
     "Draw circle from start to end."
     up()
@@ -47,19 +48,7 @@ def circle(start, end):
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
-
-    for count in range(2):
-        forward(end.x, start.x)
-        left(90)
-        forward((end.x)*2 - (start.x)*2)
-        left(90)
-
-    end_fill()
-
+    pass  # TODO
    	
 
 def triangle(start, end):
